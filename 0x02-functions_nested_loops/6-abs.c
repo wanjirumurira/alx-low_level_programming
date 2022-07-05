2543a8d8ffe3ffd49843193fd8e732263f8262be
wanjirumurira <wanjirumurira@gmail.com>
2022-07-05 13:44:39 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,19 +1,23 @@
 #include "main.h"
 /**
- * _abs - compute the absolute value of an integer
- * @n: int type number
- * Return: absolute value of @n
+ * _abs - function to get the absolute value
+ *
+ * @n: parameter of the funtion
+ *
+ * Return: the absolute value
  */
 int _abs(int n)
 {
-	if (n < 0)
-	{
-		return (n * -1);
-	}
-	else
+	if (n > 0)
 	{
 		return (n);
 	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-n);
+	}
 }
-Footer
-© 2022 GitHub, I
