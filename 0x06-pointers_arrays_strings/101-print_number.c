03d556c3da27661966393ecd26588572716e75a7
wanjirumurira <wanjirumurira@gmail.com>
2022-07-14 10:56:20 -0700

    code

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,16 +6,17 @@
  */
 void print_number(int n)
 {
-	unsigned int num = n;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		num = -num;
+		if (n < -9)
+			print_number(n / -10);
+		_putchar('0' - n % 10);
 	}
-
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+	else
+	{
+		if (n > 9)
+			print_number(n / 10);
+		_putchar(n % 10 + '0');
+	}
 }
