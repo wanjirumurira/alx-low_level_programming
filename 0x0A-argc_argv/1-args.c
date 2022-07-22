73cd2a2f8dc379d46c0ed2b8fe51f0ad90832ddb
wanjirumurira <wanjirumurira@gmail.com>
2022-07-22 13:36:00 -0700

    initial commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * @argc: arg count
+ * @argv: vector of strings with arguments
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 int main(int argc, char *argv[])
 {
 	(void)argc;
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * @argc: number of arguments
+ * @argv: vector of strings of arguments
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 int main(int argc, char *argv[])
 {
 	int i;
