017950335c5199a66205e2c2adc7884b325b58a5
wanjirumurira <wanjirumurira@gmail.com>
2022-08-01 12:22:30 -0700

    a function that prints

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ *print_dog- A function that prints struct dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description:A function that prints struct dog
+ */
+void print_dog(struct dog *d)
+{
+        if (d == NULL)
+        {
+               return (nill)
+	}
+	
+	d->name = NULL;
+	printf("Name: (nil)")
+}
+
