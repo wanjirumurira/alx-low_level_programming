657949e1a0df7e08801458a88b89ab0d2f769a2e
wanjirumurira <wanjirumurira@gmail.com>
2022-08-01 11:56:49 -0700

    A function that initialize a variable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,7 @@
  *
  * Description:A function that initialize a variable of type struct dog
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
