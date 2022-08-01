4969d76a8c639d713e0ce4f22c4926094086e512
wanjirumurira <wanjirumurira@gmail.com>
2022-08-01 11:45:00 -0700

    Initialize a variable of type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,13 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 /**
- * struct dog- Define a new type struct dog
+ * struct dog - Create the structure dog
+ *
  * @name: First member
  * @age: Second member
  * @owner: Third member
  *
- * Description:Define a new type struct dog
+ * Description: Create the structure dog
  */
 struct dog
 {
@@ -17,5 +18,10 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
